9de75e8cc6141348ece2d2e106b21f882e935a4f
Josh Northcott <jnorthcott@esri.com>
2016-07-14 10:02:12 -0700

    Added uglify and contrib to grunt

--- gruntfile.js
+++ gruntfile.js
@@ -25,6 +25,8 @@ watch: {
  //grunt.loadNpmTasks('grunt-contrib-jshint');
  grunt.loadNpmTasks('grunt-contrib-watch');
  grunt.loadNpmTasks('grunt-sass');
+ grunt.loadNpmTasks('grunt-contrib-clean');
+ grunt.loadNpmTasks('grunt-contrib-uglify');
 
  grunt.registerTask('default', ['sass', 'watch']);
 
